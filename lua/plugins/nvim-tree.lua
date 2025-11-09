2498123446f39339b9f791dd278925f65f7a7a82
Raunit <raunitarya599@gmail.com>
2025-11-10 01:48:57 +0530

    Disable nvim-tree
    Signed-off-by: Raunit <raunitarya599@gmail.com>

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,290 +1,12 @@
--- return {
--- 	-- Disable LazyVim's default file explorer
--- 	{
--- 		"snacks.nvim",
--- 		opts = {
--- 			explorer = { enabled = true },
--- 		},
--- 	},
---
--- 	-- Neo-tree configuration
--- 	{
--- 		"nvim-neo-tree/neo-tree.nvim",
--- 		branch = "v3.x",
--- 		dependencies = {
--- 			"nvim-lua/plenary.nvim",
--- 			"nvim-tree/nvim-web-devicons",
--- 			"MunifTanjim/nui.nvim",
--- 		},
--- 		cmd = "Neotree",
--- 		keys = {
--- 			{ "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Toggle Neo-tree" },
--- 			{ "<leader>ee", "<cmd>Neotree toggle<cr>", desc = "Toggle Neo-tree" },
--- 			{ "<leader>ef", "<cmd>Neotree reveal<cr>", desc = "Reveal current file in Neo-tree" },
--- 			{ "<leader>eg", "<cmd>Neotree git_status<cr>", desc = "Git status in Neo-tree" },
--- 			{ "<leader>eb", "<cmd>Neotree buffers<cr>", desc = "Show buffers in Neo-tree" },
--- 		},
--- 		deactivate = function()
--- 			vim.cmd([[Neotree close]])
--- 		end,
--- 		init = function()
--- 			vim.g.neo_tree_remove_legacy_commands = 1
--- 			if vim.fn.argc(-1) == 1 then
--- 				local stat = vim.loop.fs_stat(vim.fn.argv(0))
--- 				if stat and stat.type == "directory" then
--- 					require("neo-tree")
--- 				end
--- 			end
--- 		end,
--- 		config = function()
--- 			require("neo-tree").setup({
--- 				close_if_last_window = true,
--- 				popup_border_style = "rounded",
--- 				enable_git_status = true,
--- 				enable_diagnostics = true,
---
--- 				default_component_configs = {
--- 					container = {
--- 						enable_character_fade = true,
--- 					},
--- 					indent = {
--- 						indent_size = 2,
--- 						padding = 1,
--- 						with_markers = true,
--- 						indent_marker = "│",
--- 						last_indent_marker = "└",
--- 						highlight = "NeoTreeIndentMarker",
--- 						with_expanders = nil,
--- 						expander_collapsed = "",
--- 						expander_expanded = "",
--- 						expander_highlight = "NeoTreeExpander",
--- 					},
--- 					icon = {
--- 						folder_closed = "",
--- 						folder_open = "",
--- 						folder_empty = "󰜌",
--- 						folder_empty_open = "󰜌",
--- 						default = "",
--- 						highlight = "NeoTreeFileIcon",
--- 					},
--- 					modified = {
--- 						symbol = "[+]",
--- 						highlight = "NeoTreeModified",
--- 					},
--- 					name = {
--- 						trailing_slash = false,
--- 						use_git_status_colors = true,
--- 						highlight = "NeoTreeFileName",
--- 					},
--- 					git_status = {
--- 						symbols = {
--- 							added = "✚",
--- 							modified = "",
--- 							deleted = "✖",
--- 							renamed = "󰁕",
--- 							untracked = "",
--- 							ignored = "",
--- 							unstaged = "󰄱",
--- 							staged = "",
--- 							conflict = "",
--- 						},
--- 					},
--- 				},
---
--- 				window = {
--- 					position = "left",
--- 					width = 40,
--- 					mapping_options = {
--- 						noremap = true,
--- 						nowait = true,
--- 					},
--- 					mappings = {
--- 						["<space>"] = {
--- 							"toggle_node",
--- 							nowait = false,
--- 						},
--- 						["<2-LeftMouse>"] = "open",
--- 						["<cr>"] = "open",
--- 						["<esc>"] = "cancel",
--- 						["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
--- 						["l"] = "focus_preview",
--- 						["S"] = "open_split",
--- 						["s"] = "open_vsplit",
--- 						["t"] = "open_tabnew",
--- 						["w"] = "open_with_window_picker",
--- 						["C"] = "close_node",
--- 						["z"] = "close_all_nodes",
--- 						["Z"] = "expand_all_nodes",
--- 						["a"] = {
--- 							"add",
--- 							config = {
--- 								show_path = "none",
--- 							},
--- 						},
--- 						["A"] = "add_directory",
--- 						["d"] = "delete",
--- 						["r"] = "rename",
--- 						["y"] = "copy_to_clipboard",
--- 						["x"] = "cut_to_clipboard",
--- 						["p"] = "paste_from_clipboard",
--- 						["c"] = "copy",
--- 						["m"] = "move",
--- 						["q"] = "close_window",
--- 						["R"] = "refresh",
--- 						["?"] = "show_help",
--- 						["<"] = "prev_source",
--- 						[">"] = "next_source",
--- 						["i"] = "show_file_details",
--- 					},
--- 				},
---
--- 				nesting_rules = {},
---
--- 				filesystem = {
--- 					filtered_items = {
--- 						visible = false,
--- 						hide_dotfiles = true,
--- 						hide_gitignored = true,
--- 						hide_hidden = true,
--- 						hide_by_name = {
--- 							".DS_Store",
--- 							"thumbs.db",
--- 							"node_modules",
--- 						},
--- 						hide_by_pattern = {
--- 							"*.meta",
--- 							"*/src/*/tsconfig.json",
--- 						},
--- 						always_show = {
--- 							".gitignored",
--- 						},
--- 						never_show = {
--- 							".DS_Store",
--- 							"thumbs.db",
--- 						},
--- 						never_show_by_pattern = {
--- 							".null-ls_*",
--- 						},
--- 					},
--- 					follow_current_file = {
--- 						enabled = true,
--- 						leave_dirs_open = false,
--- 					},
--- 					group_empty_dirs = false,
--- 					hijack_netrw_behavior = "open_default",
--- 					use_libuv_file_watcher = false,
--- 					window = {
--- 						mappings = {
--- 							["<bs>"] = "navigate_up",
--- 							["."] = "set_root",
--- 							["H"] = "toggle_hidden",
--- 							["/"] = "fuzzy_finder",
--- 							["D"] = "fuzzy_finder_directory",
--- 							["#"] = "fuzzy_sorter",
--- 							["f"] = "filter_on_submit",
--- 							["<c-x>"] = "clear_filter",
--- 							["[g"] = "prev_git_modified",
--- 							["]g"] = "next_git_modified",
--- 							["o"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
--- 							["oc"] = { "order_by_created", nowait = false },
--- 							["od"] = { "order_by_diagnostics", nowait = false },
--- 							["og"] = { "order_by_git_status", nowait = false },
--- 							["om"] = { "order_by_modified", nowait = false },
--- 							["on"] = { "order_by_name", nowait = false },
--- 							["os"] = { "order_by_size", nowait = false },
--- 							["ot"] = { "order_by_type", nowait = false },
--- 						},
--- 						fuzzy_finder_mappings = {
--- 							["<down>"] = "move_cursor_down",
--- 							["<C-n>"] = "move_cursor_down",
--- 							["<up>"] = "move_cursor_up",
--- 							["<C-p>"] = "move_cursor_up",
--- 						},
--- 					},
---
--- 					commands = {},
--- 				},
---
--- 				buffers = {
--- 					follow_current_file = {
--- 						enabled = true,
--- 						leave_dirs_open = false,
--- 					},
--- 					group_empty_dirs = true,
--- 					show_unloaded = true,
--- 					window = {
--- 						mappings = {
--- 							["bd"] = "buffer_delete",
--- 							["<bs>"] = "navigate_up",
--- 							["."] = "set_root",
--- 							["o"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
--- 							["oc"] = { "order_by_created", nowait = false },
--- 							["od"] = { "order_by_diagnostics", nowait = false },
--- 							["om"] = { "order_by_modified", nowait = false },
--- 							["on"] = { "order_by_name", nowait = false },
--- 							["os"] = { "order_by_size", nowait = false },
--- 							["ot"] = { "order_by_type", nowait = false },
--- 						},
--- 					},
--- 				},
---
--- 				git_status = {
--- 					window = {
--- 						position = "float",
--- 						mappings = {
--- 							["A"] = "git_add_all",
--- 							["gu"] = "git_unstage_file",
--- 							["ga"] = "git_add_file",
--- 							["gr"] = "git_revert_file",
--- 							["gc"] = "git_commit",
--- 							["gp"] = "git_push",
--- 							["gg"] = "git_commit_and_push",
--- 							["o"] = { "show_help", nowait = false, config = { title = "Order by", prefix_key = "o" } },
--- 							["oc"] = { "order_by_created", nowait = false },
--- 							["od"] = { "order_by_diagnostics", nowait = false },
--- 							["om"] = { "order_by_modified", nowait = false },
--- 							["on"] = { "order_by_name", nowait = false },
--- 							["os"] = { "order_by_size", nowait = false },
--- 							["ot"] = { "order_by_type", nowait = false },
--- 						},
--- 					},
--- 				},
---
--- 				-- Event handlers
--- 				event_handlers = {
--- 					{
--- 						event = "file_opened",
--- 						handler = function(file_path)
--- 							require("neo-tree.command").execute({ action = "close" })
--- 						end,
--- 					},
--- 				},
--- 			})
---
--- 			-- Auto-open Neo-tree on startup for directories
--- 			vim.api.nvim_create_autocmd("BufEnter", {
--- 				group = vim.api.nvim_create_augroup("Neotree_start", { clear = true }),
--- 				callback = function()
--- 					if package.loaded["neo-tree.sources.manager"] then
--- 						return
--- 					else
--- 						local stats = vim.loop.fs_stat(vim.api.nvim_buf_get_name(0))
--- 						if stats and stats.type == "directory" then
--- 							require("neo-tree")
--- 						end
--- 					end
--- 				end,
--- 			})
--- 		end,
--- 	},
--- }
-
 return {
 	"nvim-tree/nvim-tree.lua",
-	dependencies = "nvim-tree/nvim-web-devicons",
+	enabled = true,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"nvim-mini/mini.icons",
+	},
 	config = function()
 		local nvimtree = require("nvim-tree")
-
 		-- recommended settings from nvim-tree documentation
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
@@ -300,10 +22,44 @@ return {
 					enable = true,
 				},
 				icons = {
-					glyphs = {
+					web_devicons = {
+						file = {
+							enable = true,
+							color = true,
+						},
 						folder = {
-							arrow_closed = "", -- arrow when folder is closed
-							arrow_open = "", -- arrow when folder is open
+							enable = false,
+							color = true,
+						},
+					},
+					show = {
+						file = true,
+						folder = true,
+						folder_arrow = true,
+						git = true,
+					},
+					glyphs = {
+						default = "",
+						symlink = "",
+						bookmark = "󰆤",
+						modified = "●",
+						folder = {
+							arrow_closed = "▶", -- arrow when folder is closed
+							arrow_open = "▼", -- arrow when folder is open							default = "",
+							open = "",
+							empty = "",
+							empty_open = "",
+							symlink = "",
+							symlink_open = "",
+						},
+						git = {
+							unstaged = "✗",
+							staged = "✓",
+							unmerged = "",
+							renamed = "➜",
+							untracked = "★",
+							deleted = "",
+							ignored = "◌",
 						},
 					},
 				},
@@ -325,18 +81,16 @@ return {
 				ignore = false,
 			},
 		})
-
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
-
-		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
 		keymap.set(
 			"n",
 			"<leader>ef",
 			"<cmd>NvimTreeFindFileToggle<CR>",
 			{ desc = "Toggle file explorer on current file" }
-		) -- toggle file explorer on current file
-		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+		)
+		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
 	end,
 }
