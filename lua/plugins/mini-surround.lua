ebc50a0faac5bc252f2adbf69904d67c6359afd6
Raunit <raunitarya599@gmail.com>
2025-11-28 19:18:40 +0530

    Remap mini-surround
    Signed-off-by: Raunit <raunitarya599@gmail.com>

---
+++ lua/plugins/mini-surround.lua
@@ -0,0 +1,14 @@
+return {
+	"nvim-mini/mini.surround",
+	opts = {
+		mappings = {
+			add = "gza", -- Add surrounding
+			delete = "gzd", -- Delete surrounding
+			replace = "gzr", -- Replace surrounding
+			find = "gzf",
+			find_left = "gzF",
+			highlight = "gzh",
+			update_n_lines = "gzn",
+		},
+	},
+}
