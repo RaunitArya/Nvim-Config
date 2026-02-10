9ce5f75f1a9ee09b17f0e6f099e4a4c0e8d77e7a
Raunit <raunitarya599@gmail.com>
2026-02-10 11:13:06 +0530

    Make colorscheme transparent
    Signed-off-by: Raunit <raunitarya599@gmail.com>

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,48 @@
 return {
-	-- add gruvbox
-	{ "navarasu/onedark.nvim" },
+	{
+		"navarasu/onedark.nvim",
+		priority = 1000,
+		config = function()
+			require("onedark").setup({
+				style = "darker",
 
-	-- Configure LazyVim to load gruvbox
+				transparent = true,
+
+				lualine = {
+					transparent = true,
+				},
+
+				--  Custom highlight overrides for floats & sidebars
+				highlights = {
+					Normal = { bg = "none" },
+					NormalNC = { bg = "none" },
+					NormalFloat = { bg = "none" },
+					FloatBorder = { bg = "none" },
+					FloatTitle = { bg = "none" },
+					SignColumn = { bg = "none" },
+					StatusLine = { bg = "none" },
+					StatusLineNC = { bg = "none" },
+					TabLineFill = { bg = "none" },
+					NeoTreeNormal = { bg = "none" },
+					NeoTreeNormalNC = { bg = "none" },
+					Pmenu = { bg = "none" }, -- completion menu
+					PmenuSel = { bg = "none" },
+					PmenuSbar = { bg = "none" },
+					PmenuThumb = { bg = "none" },
+				},
+			})
+
+			require("onedark").load()
+		end,
+	},
+
+	{ "projekt0n/github-nvim-theme", name = "github-theme" },
+
+	-- LazyVim selects the theme
 	{
 		"LazyVim/LazyVim",
 		opts = {
 			colorscheme = "onedark",
-			style = "darker",
 		},
 	},
 }
