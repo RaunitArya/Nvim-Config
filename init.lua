72b61f0ac8547e4bf1c842686d8ce7b60668f930
Raunit <raunitarya599@gmail.com>
2026-02-10 11:16:20 +0530

    Always show bufferline
    Signed-off-by: Raunit <raunitarya599@gmail.com>

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+vim.g.python3_host_prog = "C:/Users/Raunit/AppData/Local/Programs/Python/Python312/python.exe"
 require("config.lazy")
 -- vim.opt.relativenumber = false
 
